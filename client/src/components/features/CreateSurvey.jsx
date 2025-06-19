54a8ebaae50e3842c0de629e8cb318112a436e94
ENGAbderahmanZaghloul <114342047+ENGAbderahmanZaghloul@users.noreply.github.com>
2025-06-19 04:05:49 +0300

    finish feature page , build 3 sections with responsive , [action banner , customer needs , create survey] .. abdu

---
+++ client/src/components/features/CreateSurvey.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const CreateSurvey = () => {
+  return (
+    <section className=" mb-40 h-[300px] bg-gradient-to-r from-purple via-purple/90 to-midnight text-white flex flex-col gap-4 items-center justify-center text-center">
+      <h1 className="text-xl font-bold">Ready to Create Your First Survey?</h1>
+      <p className="text-base font-extralight">
+        Join thousands of professionals who trust SurveyPro for their research
+        needs.
+      </p>
+      <div className="flex flex-col sm:flex-row gap-3">
+        <button className="text-purple bg-white py-2 px-6 rounded-lg">Start Free Trial</button>
+        <button className="border border-white py-2 px-6 rounded-lg">Contact Sales</button>
+      </div>
+    </section>
+  );
+};
+
+export default CreateSurvey;
--- client/src/pages/Feature.jsx
+++ client/src/pages/Feature.jsx
@@ -1,13 +1,20 @@
-
+import ActionBanner from "../components/features/ActionBanner";
+import CreateSurvey from "../components/features/CreateSurvey";
+import CustomerNeeds from "../components/features/CustomerNeeds";
 import { FeaturesList } from "../components/features/FeaturesList";
 import HeroSection from "../components/features/HeroSection";
 
 const Feature = () => {
   return (
-    <section className="container mx-auto px-5">
-      <HeroSection />
-      <FeaturesList/>
-    </section>
+    <>
+      <section className="container mx-auto px-5">
+        <HeroSection />
+        <FeaturesList />
+        <CustomerNeeds />
+        <ActionBanner />
+      </section>
+      <CreateSurvey />
+    </>
   );
 };
 
